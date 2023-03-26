e925dbc4c24b0b256f55615f730d676ea4c6398d
karisDev <karjaakirill@gmail.com>
2023-03-26 18:19:07 +0300

    svg as component, d.ts, navbar

--- frontend/src/pages/Home/TopBar/index.tsx
+++ frontend/src/pages/Home/TopBar/index.tsx
@@ -1,5 +1,22 @@
+import cl from "./styles.module.scss";
+import { ReactComponent as LogoFull } from "./assets/icons/kion_full.svg";
+import { ReactComponent as User } from "./assets/icons/user.svg";
+
 const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className={cl.nav__wrapper}>
+      <div className={cl.nav}>
+        <LogoFull className={cl.nav__logo} />
+        <h1>TopBar</h1>
+        <div className={cl.nav__right}>
+          <div className={cl.right__profile}>
+            <User className={cl.profile__avatar} />
+            <span>профиль</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
